9e905534defdbe6da368d7d30c78e3032fd252d7
Abu Daud <a.kulametov@MacBook-Pro-Abu.local>
2023-11-08 21:30:07 +0100

    second commit

---
+++ js/script.js
@@ -0,0 +1,48 @@
+
+
+//this is button for modal for user login and password
+
+const modal = document.querySelector('.modal');
+document.body.addEventListener('click', e => {
+    if(e.target.classList.contains('modal-close-img')){
+        modal.style.display = 'none';
+    } else if(e.target.classList.contains('modal-container')){
+        modal.style.display = 'none';
+    } else if(e.target.classList.contains('menu__link-active')){
+        modal.style.display = 'block';
+    }
+});
+
+// this ist function for cards
+
+class cards {
+    constructor(imgSrc, altImg, title, description) {
+      this.imgSrc = imgSrc;
+      this.altImg = altImg;
+      this.title = title;
+      this.description = description;
+    }
+  
+    addCards() {
+        const div = document.querySelector('.cards-container');
+        const newDiv = document.createElement('div');
+        newDiv.innerHTML = `
+        <div class="card">
+            <img src=${this.imgSrc} alt=${this.altImg} class="card-img">
+            <h2 class="card-title">${this.title}</h2>
+            <p class="card-description my-description-class">${this.description}</p>    
+        </div>`;
+        div.append(newDiv);
+    }
+};
+
+const arr = [];
+fetch('http://localhost:3000/menu')
+.then(data => data.json())
+.then(data => {
+   data.forEach(({img,altimg,title,descr}) => {
+        new cards(img,altimg,title,descr).addCards()
+   })
+})
+
+/* swiper */
